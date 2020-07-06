f311bd4329060a735fa8a9968c788608a0257a41
gangadevi <kakarlagangadevi@gmail.com>
2020-07-06 23:49:28 +0530

    Daily Employee Wage calculated for full time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
-echo "Welcome"
+isPresent=1
+isAbsent=0
+empWageperHour=20
+isFulltime=8
+empCheck=$((RANDOM%2))
+if [[ empCheck -eq 1 ]]
+then
+	salary=$(($empWageperHour * $isFulltime))
+	echo "Employee Daily wage" $salary
+else
+	echo "Employee Absent"
+fi
