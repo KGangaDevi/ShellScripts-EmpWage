a0bc9535ff254628a95ee7bcd5a060904ca066ea
gangadevi <kakarlagangadevi@gmail.com>
2020-07-07 10:01:59 +0530

    Employee Monthly Wage Calculated

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,17 +2,22 @@
 isPartTime=1
 isFullTime=2
 empWageperHour=20
-empCheck=$((RANDOM%3))
-case $empCheck in
-	$isFullTime)
+workdays=20
+for((i=1;i<=$workdays;i++))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		$isFullTime)
 		empHours=8
 		;;
-	$isPartTime)
+		$isPartTime)
 		empHours=4
 		;;
-	*)
+		*)
 		empHours=0
 		;;
-esac
-salary=$(($empWageperHour * $empHours))
-echo "Employee Daily wage" $salary
+	esac
+	totalempHrs=$(($totalempHrs+$empHours))
+done
+salary=$(($empWageperHour * $totalempHrs))
+echo "Employee Monthly wage" $salary
