3f4c0e2dcf9bec0bece72944ad29c53a3c9e5a5d
gangadevi <kakarlagangadevi@gmail.com>
2020-07-07 09:36:36 +0530

    Employee wage full-time, part-time calculated using case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,14 +3,16 @@ isPartTime=1
 isFullTime=2
 empWageperHour=20
 empCheck=$((RANDOM%3))
-if [[ $isFullTime -eq $empCheck ]]
-then
-	empHours=8
-elif [[ $isPartTime -eq $empCheck ]]
-then
-	empHours=4
-else
-	empHours=0
-fi
+case $empCheck in
+	$isFullTime)
+		empHours=8
+		;;
+	$isPartTime)
+		empHours=4
+		;;
+	*)
+		empHours=0
+		;;
+esac
 salary=$(($empWageperHour * $empHours))
 echo "Employee Daily wage" $salary
