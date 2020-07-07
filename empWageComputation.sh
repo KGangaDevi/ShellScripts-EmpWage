37e2f9db402428cbc4c17e9216e37b718334a21a
gangadevi <kakarlagangadevi@gmail.com>
2020-07-07 11:05:07 +0530

    Employee Wage using getWorkHours Function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,22 +6,28 @@ maxworkDays=20
 maxworkHours=100
 totalWorkingHours=0
 totalWorkingDays=0
+function getWorkHours()
+{
+	local $empCheck=$1
+	 case $empCheck in
+                $isFullTime)
+                empHours=8
+                ;;
+                $isPartTime)
+                empHours=4
+                ;;
+                *)
+                empHours=0
+                ;;
+        esac
+	echo $empHours
+}
 while [[ $totalWorkingHours -lt $maxworkHours && $totalWorkingDays -lt $maxworkDays ]]
 do
 	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$isFullTime)
-		empHours=8
-		;;
-		$isPartTime)
-		empHours=4
-		;;
-		*)
-		empHours=0
-		;;
-	esac
+	empHours="$(getWorkHours $empCheck)"
 	totalWorkingHours=$(($totalWorkingHours+$empHours))
 done
 salary=$(($empWageperHour * $totalWorkingHours))
-echo "Employee Monthly wage based on Max working hours" $salary
+echo "Employee Monthly wage using function for working hours " $salary
